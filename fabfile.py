8c5c0a8ce2a8add57811ecdb09c3d3e1182ecd64
the-lans <ilyav87@gmail.com>
2022-10-24 00:39:38 +0300

    SQL: psql

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,11 @@ from fabric import task
 
 
 @task
-def hello(c, name="world"):
-    print("Hello %s!" % name)
-    c.run("uname -a")
+def hello(c, name='world'):
+    print('Hello %s!' % name)
+    c.run('uname -a')
+
+
+@task
+def sql(c, com, name='my_db'):
+    c.run(f"export PATH=/usr/pgsql-10/bin:$PATH && psql -U postgres -d {name} -c '{com}'")
